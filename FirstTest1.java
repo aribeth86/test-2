674c4f68bbca19ead9c0edebc1482be5e864cd0b
aribeth86 <aribeth@list.ru>
2021-07-27 18:34:02 +0700

    Update FirstTest1.java

--- FirstTest1.java
+++ FirstTest1.java
@@ -1,5 +1,13 @@
 public class FirstTest1 {
-  public static void main(String[] args){
-    System.out.println ("We are here! Hello!");
+
+  public static void main (String[] args) {
+      System.out.println ("We are here! Hello!");
+
+      System.out.println (8 + 2);
+      System.out.println (8 - 2);
+      System.out.println (8 * 2);
+      System.out.println (8 / 3);
+
+      System.out.println (8.0 / 3);
   }
- }
+}
